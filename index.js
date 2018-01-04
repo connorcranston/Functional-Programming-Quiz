0b9b16d8207139dc8516038629c41262dc776ee8
connorcranston <connorcranston@gmail.com>
2018-01-03 20:19:16 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+var arr = [0,1,2,3,4,5],
+
+var newarr =
+
+const objects = [
+    {name: "Pizza"},
+    {name: "Cars"},
+    {name: "Clothes"}
+]
+
+class Properties{
+    constructor(payroll){
+        this.paryoll = payroll
+        this.extraCreditnames = this.giveExtraCredit(this.Names)
+        this.rate = this.getPassers(this.rate)
+        this.worked = this.getWorked(this.Worked)
+    }
+
+    getNames(Name){
+        return newarray.sort(name)=>{
+            return  name.score 
+        })
+    }
+
+    getFullTime(Rate){
+        return rates.filter((rate)=>{
+            return (rate > 60) ? 1 : 0
+        })
+    }
+
+    getTotalLabor(Worked){
+        return worked.reduce((worked, rate) =>{
+            return (index === worked.length) ? (worked * rate) : (acc + cur)
+        })
+    }
+}
+
+var test = new Properties(payroll)
+console.log(test.worked)
